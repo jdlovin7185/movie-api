4360bf6ad7f4cc8cdfaf92f34fd93eb00bcfbe41
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-02-18 11:16:40 -0500

    files

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 const express = require("express"),
-  morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('common'));
+let topMovies = [
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'Interstellar',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'Once Upon a Time in Hollywood',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'Inglorious Basterds',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'The Lincoln Lawyer',
+    director: 'Brad Furman'
+  },
+  {
+    title: 'L.A. Confidential',
+    director: 'Curtis Hanson'
+  },
+  {
+    title: 'No Country for Old Men',
+    director: 'Ethan Coen'
+  },
+  {
+    title: 'Ford v Ferrari',
+    director: 'James Mangold'
+  },
+  {
+    title: 'The Departed',
+    director: 'Martin Scorsese'
+  }
+];
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+  res.send('Welcome to my movie collection!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret place with secret stuff');
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: _dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
 });
 
 app.listen(8080, () => {
