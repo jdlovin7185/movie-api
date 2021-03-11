5058acafb91782906afd99f377eb2562eea82719
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-03-11 12:10:07 -0500

    More bugs

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect( process.env.CONNECTION_URI,
   let auth = require('./auth')(app);
   require('./passport');
   
-  let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+  let allowedOrigins = ['http://localhost:8080', 'https://myflix1-0.herokuapp.com','http://localhost:1234'];
   app.use(cors({
     origin: (origin, callback) => {
       if(!origin) return callback(null, true);
