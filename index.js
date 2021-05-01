0c4c63f0caf3d551fe0233190ad7d882cb319ada
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-05-01 14:04:13 -0400

    adds a localhost connect

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect( process.env.CONNECTION_URI,
 
   require('./passport');
   
-  const allowedOrigins = ['http://localhost:8080', 'https://myflix1-0.herokuapp.com/', 'http://localhost:1234'];
+  const allowedOrigins = ['http://localhost:8080', 'https://myflix1-0.herokuapp.com/', 'http://localhost:1234', 'http://localhost:4200'];
   app.use(cors({
     origin: (origin, callback) => {
       if(!origin) return callback(null, true);
