4c07cadda793b877ddb74f0fccfc796e79ce3581
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-05-10 14:42:44 -0400

    bug

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ app.post('/users',
   let errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({errors: errors.array(message) });
+    return res.status(422).json({errors: errors.array() });
   }
   if (!errors.Username)
   let hashedPassword = Users.hashPassword(req.body.Password);
